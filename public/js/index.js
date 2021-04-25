a6f2aeb8cfd95fb985745bdd643787c120438ffe
Jason Wilkes <jasonbwilkes@icloud.com>
2021-04-24 19:05:36 -0700

    v1.0 ready to realease

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 // variable 'trainers' passed in from EJS
+const d = document;
 const trainerList = new DocumentFragment();
-
+const submitButton = d.getElementById('submit-button');
+const backButton = d.getElementById('back-button');
+const copyButton = d.getElementById('copy-button');
+const chosenOnesWindow = d.querySelector('.chosen-ones-container');
 
 function compare(a, b) {
     const trainerA = a.trainer.toLowerCase();
@@ -14,21 +18,32 @@ function compare(a, b) {
 
 trainers.sort(compare);
 
+function copyText() {
+    const text = d.getElementById('the-chosen');
+    text.select();
+    text.setSelectionRange(0, 99999); /* For mobile devices */
+    d.execCommand('copy');
+    alert('Copied! Now go paste into Pokémon GO.');
+    chosenOnesWindow.classList.add('hidden');
+    document.getSelection().removeAllRanges();
+}
+
+
 // create a new trainer card with a trainer object
 function createTrainerCard(data) {
     // define trainer details from data
     const thisName = data.trainer;
-    const thisTeam = (data.team === undefined) ? 'harmony' : data.team;
+    const thisTeam = (data.team === undefined || data.team === null || data.team ==='') ? 'harmony' : data.team;
     const thisFriendCode = (data.friendCode === undefined) ? '???? ???? ????': data.friendCode;
 
     // create trainer code elements
-    const checkbox = document.createElement('div');
-    const friendCode = document.createElement('span');
-    const team = document.createElement('div');
-    const teamLogo = document.createElement('img');
-    const trainerCard = document.createElement('div');
-    const trainerInfo = document.createElement('div');
-    const trainerName = document.createElement('span');
+    const checkbox = d.createElement('div');
+    const friendCode = d.createElement('span');
+    const team = d.createElement('div');
+    const teamLogo = d.createElement('img');
+    const trainerCard = d.createElement('div');
+    const trainerInfo = d.createElement('div');
+    const trainerName = d.createElement('span');
 
     // set styles and attributes of trainer card elements
     checkbox.classList.add('checkbox');
@@ -60,16 +75,33 @@ trainers.forEach((item, index, arr) => {
 });
 
 // add new trainer list (document fragment) to main window
-document.querySelector('.main-window').appendChild(trainerList);
+d.querySelector('.main-window').appendChild(trainerList);
 
 // get list of all trainer cards
-const trainerCards = document.querySelectorAll('.trainer-card');
+const trainerCards = d.querySelectorAll('.trainer-card');
 
 // add event listeners to each trainer card
 trainerCards.forEach((x, i, a) => {
     x.addEventListener('click', function () {
         x.classList.toggle('selected');
-        const counter = document.querySelectorAll('.selected').length;
-        document.querySelector('.counter').innerText = counter;
+        const counter = d.querySelectorAll('.selected').length;
+        d.querySelector('.counter').innerText = counter;
     });
+});
+
+/********** LISTENERS **********/
+backButton.addEventListener('click', () => {
+    chosenOnesWindow.classList.add('hidden');
+});
+
+copyButton.addEventListener('click', copyText);
+
+submitButton.addEventListener('click', () => {
+    const selected = Array.from(d.querySelectorAll('.selected'));
+    const chosenOnes = selected.map(x => {
+        return x.children[1].children[0].innerHTML;
+    });
+    const searchString = chosenOnes.join(', ');
+    d.getElementById('the-chosen').value = searchString;
+    chosenOnesWindow.classList.remove('hidden');
 });
